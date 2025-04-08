c156e6a500dc82a6e76b901dced9e3138d88e0c7
Lucy Alves <alveslucy348@gmail.com>
2025-04-07 21:20:32 -0300

    uma msg

--- todo/views.py
+++ todo/views.py
@@ -1,12 +1,21 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from .models import Task
+import json
 # Create your views here.
 
 def update (request, task_id) :
     return JsonResponse ({'error' : 'Invalid data'} , status=400)
+
+
 def listar(request) :
     if request.method == "GET" :
         tasks = Task.objects.all()
-        return render(request, 'index.html' , {'tasks' : tasks})
-    
+        return render(request, 'index.html' , {'tasks' : tasks}) 
+    if request.method == "POST":
+        data = json.loads(request.body)
+        form = TaskForm(data)
+        if form.is_valid():
+            task = form.save()
+            return JsonResponse({'id': task.id, 'title': task.title, 'completed': task.completed}, status=201)
+        return JsonResponse({'error': 'Invalid data'}, status=400)
